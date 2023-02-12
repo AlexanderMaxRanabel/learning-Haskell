ee67d483fb1dfb021b26b7cafe72fedde98b4ce8
AlexanderMaxRanabel <93342430+AlexanderMaxRanabel@users.noreply.github.com>
2023-02-12 23:05:25 +0300

    Create main.hs

---
+++ main.hs
@@ -0,0 +1,6 @@
+packagerOfHaskell = "Cabal"
+haskellInventionDate = 1987
+
+main = do
+  putStrLn("Package mangaer of haskell is " ++ packagerOfHaskell)
+  print("Haskell's İnvention date is " ++ show haskellInventionDate)
